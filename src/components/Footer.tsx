21a8ae0e1baf6eae547d1287da6852a49c22a6ec
ousama <ousawack@gmail.com>
2023-06-07 00:24:25 +0100

    styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+/* eslint-disable @next/next/no-img-element */
+export default function Footer() {
+  return (
+    <a
+      href="https://www.ousama.vercel.app/"
+      target={"_blank"}
+      rel="noreferrer"
+      className="absolute bottom-0 flex w-full items-center justify-center gap-2 p-6 font-sans text-sm font-semibold text-orange-200 md:w-2/3 md:p-4 lg:w-1/2 "
+    >
+      Made With {`❤️`} By Me
+    </a>
+  );
+}
