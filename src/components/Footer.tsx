7c2afc69c16c4014b41109aed36fbe6a15156d5c
ousama <ousawack@gmail.com>
2023-06-07 00:39:09 +0100

    re-styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
       href="https://www.ousama.vercel.app/"
       target={"_blank"}
       rel="noreferrer"
-      className="absolute bottom-0 flex w-full items-center justify-center gap-2 p-6 font-sans text-sm font-semibold text-orange-200 md:w-2/3 md:p-4 lg:w-1/2 "
+      className="absolute bottom-0 flex w-full items-center justify-center gap-2 p-6 font-beth text-sm font-normal text-orange-100 md:w-2/3 md:p-4 lg:w-1/2 "
     >
-      Made With {`❤️`} By Me
+      made with {`❤️`} by ousama
     </a>
   );
 }
