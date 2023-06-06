12d401220be9d0c3c9f0fc31ea88ef707faa82fc
ousama <ousawack@gmail.com>
2023-06-07 00:38:55 +0100

    font config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", "sans-serif"],
+        mono: ["Space Mono", "monospace"],
+        beth: ["Beth Ellen", "beth-ellen"],
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar")],
 } satisfies Config;
