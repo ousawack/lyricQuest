ca44b74607433d6118b353da7baf7000c872cad2
ousama <ousawack@gmail.com>
2023-06-07 00:25:20 +0100

    meta tags

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Head from "next/head";
 
-const Meta = () => {
-  return <div>Meta</div>;
-};
-
-export default Meta;
+export default function Meta() {
+  return (
+    <Head>
+      <title>LyricQuest</title>
+      <link rel="icon" href="https://fmj.asrvd.me/🎶"></link>
+      <meta
+        name="description"
+        content="Discover the Lyrics to Your Favorite Songs!"
+      />
+      <meta name="theme-color" content="#fce7f3" />
+      <meta property="og:site_name" content="LyricQuest" />
+      <meta property="og:title" content="LyricQuest" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="simple yet powerful RESTful lyrics API"
+      />
+    </Head>
+  );
+}
